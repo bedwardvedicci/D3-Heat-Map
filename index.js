79def192dc0f88d9b9ff401c50c1fa783e9beb3e
bedwardvedicci <76665355+bedwardvedicci@users.noreply.github.com>
2021-02-21 10:19:03 +0100

    rebuilt the Map's legend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import * as d3 from "d3";
 import "./index.css";
 
+
 window.onload = async () => {
   const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
   const data = await fetch(url);
@@ -35,8 +36,8 @@ const init = (exData) => {
   }
   
   const data = exData.monthlyVariance; // {year, month, variance}
-  const base = toDec(exData.baseTemperature);
-  data.forEach((o)=> o.variance = toDec(o.variance));
+  const base = exData.baseTemperature;
+  data.forEach((o)=> o.variance = o.variance);
   // CONSTANTS ↑↑↑
   
   const mapSvg = d3
@@ -53,13 +54,7 @@ const init = (exData) => {
                   .domain(data.map(o=>o.year)) // array of Years
                   .range([pad.w, w-pad.e])
                   .paddingOuter(.1)
-                  .align(1);
-  
-  const xAxisScale = d3
-                      .scaleLinear()
-                      .domain([d3.min(data, d=>d.year), d3.max(data, d=>d.year)])
-                      .range([pad.w, w-pad.e]);
-                  
+                  .align(1);       
   
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   data.forEach(o=>{
@@ -70,22 +65,28 @@ const init = (exData) => {
                   .domain(data.map(o=>o.month))
                   .range([pad.n, h-pad.s]);
   
-                  
-  const minTmp=2.8; // Temperature
-  const maxTmp=12.8;
-  const colorScale = d3
-                      .scaleQuantize()
-                      .domain([minTmp, maxTmp])
-                      .range(["#4575b4","#74add1","#abd9e9","#e0f3f8","#ffffbf", "#fee090", "#fdae61", "#f46d43", "#d73027"]);
-  
+  const colors = ["#3060a4","#4575b4","#74add1","#abd9e9","#e0f3f8","#ffffbf","#fee090","#fdae61","#f46d43","#d73027","#c71512"]
+  const temperatures = data.map(d=>d.variance+base);
+  const minTmp = Math.min.apply(null, temperatures); // min Temperature
+  const maxTmp = Math.max.apply(null, temperatures); // max Temperature
+  const thresholdScale = d3
+                      .scaleThreshold()
+                      .domain(((min, max, count)=>{// (max-min / colors count) to get the step
+                        const thresholds = [];
+                        const step = (max-min)/count;
+                        const base = min;
+                        for (let i=1; i<count; i++) {
+                          thresholds.push(base+step*i)
+                        }
+                        return thresholds; // return array of (colors count -1) of thresholds
+                      })(minTmp, maxTmp, colors.length))
+                      .range(colors);
   // ↑↑↑ Scales
   
   // ↓↓↓ Axis
-  
   const xAxis = d3
-                .axisBottom(xAxisScale)
-                .tickFormat(d3.format("d"))
-                .ticks(20)
+                .axisBottom(xScale)
+                .tickValues(xScale.domain().filter(y => y%10===0))
                 .tickSizeOuter(0);
   
   const yAxis = d3
@@ -104,9 +105,10 @@ const init = (exData) => {
     .attr("transform", `translate(${pad.w}, 0)`)
     .call(yAxis);
   // ↑↑↑ Axis
-  
+
   // MouseHandlers ↓↓↓
   const handleMouseOver = (e, d) => {
+
     const temp = toDec(base+d.variance);
     
     d3
@@ -132,7 +134,6 @@ const init = (exData) => {
   }
   // MouseHandlers ↑↑↑
   
-  
   mapSvg
     .selectAll(".cell")
     .data(data)
@@ -143,52 +144,57 @@ const init = (exData) => {
     .attr("width", xScale.bandwidth())
     .attr("y", d=>yScale(d.month))
     .attr("height", yScale.bandwidth())
-    .attr("fill", d=>colorScale(toDec(base+d.variance)))
+    .attr("fill", d=>thresholdScale(base+d.variance))
     .attr("data-month", d=>months.indexOf(d.month))
     .attr("data-year", d=>d.year)
-    .attr("data-temp", d=>toDec(base+d.variance))
+    .attr("data-temp", d=>base+d.variance)
     .on("mouseover", handleMouseOver)
     .on("mouseout", handleMouseOut);
   
-  // Map's Legend ↓↓↓  
-  const thresholds = [...colorScale.thresholds()].map(v=>toDec(v));
-  thresholds.unshift(minTmp);
-  thresholds.push(maxTmp);
+  //Map's Legend ↓↓↓  
+  const lW=300, lH=70, lHPad=15, lVPad=25; // legend's width and height.
   
-  const lW=300, lH=65, lHPad=15, lVPad=20; // legend's width and height.
-  const sqDimens = (lW-(lHPad*2))/(thresholds.length-1);// sqDimens for square Dimensions
-  
-  const legend = d3
+  const legendSvg = d3
     .select("#map")
     .append("svg")
     .attr("id", "legend")
     .attr("width", lW)
     .attr("height", lH);
-  
-  const legendScale = d3.scalePoint()
-                            .domain(thresholds)
-                            .range([lHPad, lW-lHPad]);
+
+  const legendXScale = d3
+                        .scaleLinear()
+                        .domain([minTmp, maxTmp])
+                        .range([lHPad, lW-lHPad]);
   
   const legendAxis = d3
-                      .axisBottom(legendScale)
+                      .axisBottom(legendXScale)
+                      .tickValues(thresholdScale.domain())
+                      .tickFormat(d3.format(".1f"))
+                      .tickSize(13)
                       .tickSizeOuter(0);
+
+  const gLegendAxis = legendSvg
+                        .append("g")
+                        .attr("transform", `translate(0, ${lH-lVPad})`)
+                        .call(legendAxis);
   
-  legend
-      .append("g")
-      .attr("transform", `translate(0, ${lH-lVPad})`)
-      .call(legendAxis);
-  
-  legend
+  gLegendAxis
       .selectAll(".legendRect")
-      .data(thresholds.slice(0, thresholds.length-1))
+      .data(thresholdScale.range().map(color => {
+        const delim = thresholdScale.invertExtent(color); // delimiter
+        if (!delim[0]) delim[0]=legendXScale.domain()[0]; // if delimiter has a falsy value, null or undefined
+        if (!delim[1]) delim[1]=legendXScale.domain()[1];
+        return delim;
+      }))
       .enter()
-      .append("rect")
+      .insert("rect", ".tick")
       .attr("stroke", "black")
       .attr("stroke-width", "1px")
-      .attr("width", sqDimens)
-      .attr("height", sqDimens)
-      .attr("fill", d=>colorScale(d))
-      .attr("x", d=>legendScale(d))
-      .attr("y", lH-lVPad-sqDimens);
+      .attr("x", d=>legendXScale(d[0]))
+      .attr("width", d=>legendXScale(d[1])-legendXScale(d[0]))
+      .attr("y", d=>-(legendXScale(d[1])-legendXScale(d[0])))
+      .attr("height", d=>legendXScale(d[1])-legendXScale(d[0]))
+      .attr("fill", d=>thresholdScale(d[0]));
   // Map's Legend ↑↑↑
 }
+init(exData);
